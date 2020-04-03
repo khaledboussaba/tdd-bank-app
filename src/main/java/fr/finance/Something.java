c7287b46fbe8d0ddea796e0afe6836e192d40ff7
khaled <khaledboussaba@gmail.com>
2020-04-03 12:02:18 +0200

    First commit: Initialization

---
+++ src/main/java/fr/finance/Something.java
@@ -0,0 +1,4 @@
+package fr.finance;
+
+public class Something {
+}
---
+++ src/test/java/fr/finance/SomethingTest.java
@@ -0,0 +1,14 @@
+package fr.finance;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SomethingTest {
+
+    @Test
+    public void testSomthing() {
+
+    }
+
+}
