5d4cf146d0ed5d7447926b270c88a48f6070eb55
khaled <khaledboussaba@gmail.com>
2020-04-03 18:41:48 +0200

    introduce principle funds and taxes for interests and adding some tests for that

--- src/main/java/fr/finance/SavingsAccountYear.java
+++ src/main/java/fr/finance/SavingsAccountYear.java
@@ -4,6 +4,8 @@ public class SavingsAccountYear {
 
     private double startingBalance;
     private double interestRate;
+    private double capitalGainsAmount;
+    private double totalWithdrawn;
 
     public SavingsAccountYear() {
     }
@@ -13,20 +15,40 @@ public class SavingsAccountYear {
         this.interestRate = interestRate;
     }
 
+    public SavingsAccountYear(double startingBalance, double capitalGainsAmount, double interestRate) {
+        this.startingBalance = startingBalance;
+        this.capitalGainsAmount = capitalGainsAmount;
+        this.interestRate = interestRate;
+    }
+
     public double startingBalance() {
         return startingBalance;
+    }
+
+    public double startingPrinciple() {
+        return startingBalance - capitalGainsAmount;
     }
 
     public double interestRate() {
         return interestRate;
     }
 
+    public double endingPrinciple() {
+        double result = startingPrinciple() - totalWithdrawn;
+        return (result < 0) ? 0 : result;
+    }
+
     public double endingBalance() {
-        return startingBalance + (startingBalance * interestRate) / 100;
+        double modifiedStart = startingBalance - totalWithdrawn;
+        return modifiedStart + (modifiedStart * interestRate) / 100;
     }
 
     public SavingsAccountYear nextYear() {
         return new SavingsAccountYear(this.endingBalance(), interestRate);
+    }
+
+    public void withdraw(double amount) {
+        this.totalWithdrawn += amount;
     }
 
 }
--- src/test/java/fr/finance/SavingsAccountYearTest.java
+++ src/test/java/fr/finance/SavingsAccountYearTest.java
@@ -33,6 +33,52 @@ public class SavingsAccountYearTest {
         assertEquals("", thisYear.interestRate(), thisYear.nextYear().interestRate(), 0.0000);
     }
 
+    @Test
+    public void withdrawingFundsAtTheBeginingOfTheYear() {
+        SavingsAccountYear year = new SavingsAccountYear(10000, 10);
+        year.withdraw(1000);
+        assertEquals("", 9900, year.endingBalance(), 0.0000);
+    }
+
+    @Test
+    public void multipleWithdrawalsInAYear() {
+        SavingsAccountYear year = new SavingsAccountYear(10000, 10);
+        year.withdraw(1000);
+        year.withdraw(2000);
+        assertEquals("", 7700, year.endingBalance(), 0.0000);
+    }
+
+    @Test
+    public void startingPrinciple() {
+        SavingsAccountYear year = new SavingsAccountYear(10000, 7000, 10);
+        assertEquals("", 3000, year.startingPrinciple(), 0.0000);
+    }
+
+    @Test
+    public void endingPrinciple() {
+        SavingsAccountYear year = new SavingsAccountYear(10000, 7000, 10);
+        assertEquals("starting principal", 3000, year.startingPrinciple(), 0.0000);
+        year.withdraw(2000);
+        assertEquals("ending principal", 1000, year.endingPrinciple(), 0.0000);
+    }
+
+    @Test
+    public void endingPrincipleNeverGoesBelowZero() {
+        SavingsAccountYear year = new SavingsAccountYear(10000, 7000, 10);
+        assertEquals("starting principal", 3000, year.startingPrinciple(), 0.0000);
+        year.withdraw(4000);
+        assertEquals("ending principal", 0, year.endingPrinciple(), 0.0000);
+    }
+
+//    @Test
+//    public void withdrawingMoreThanPrincipalIncursCapitalGainsTax() {
+//        SavingsAccountYear year = new SavingsAccountYear(10000, 7000, 10);
+//        year.withdraw(3000);
+//        assertEquals("", 7700, year.endingBalance(), 0.0000);
+//        year.withdraw(5000);
+//        assertEquals("", 2000 + 200 - (5000 * .25), year.endingBalance(), 0.0000);
+//    }
+
     private SavingsAccountYear newAccount() {
         SavingsAccountYear account = new SavingsAccountYear(10000, 10);
         return account;
